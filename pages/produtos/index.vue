be939af83333a11e0035f713fae49d3b8d840bcc
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2022-11-09 01:45:52 -0300

    #3 - Páginas e rotas 2

--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -10,7 +10,9 @@
         
         <div>
             <div class="container mx-auto">
-                <h1 class="text-lg font-bold">A empresa</h1>
+                <h1 class="text-lg font-bold">Produtos</h1>
+                <NuxtLink to="/produtos/produto-a">Produto A</NuxtLink>
+                <NuxtLink to="/produtos/produto-b">Produto B</NuxtLink>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus diam vitae ante tincidunt, et blandit tellus pharetra. Suspendisse lobortis enim in orci egestas consectetur. Pellentesque a hendrerit nulla. Sed pulvinar nunc diam, ut blandit diam tristique vel. Suspendisse eu aliquet justo. In malesuada odio quis fermentum varius. Vestibulum tempus, sem eget consequat commodo, ex magna consequat dui, et ultrices libero erat ac dui. </p>
             </div>
         </div>
--- pages/servicos/index.vue
+++ pages/servicos/index.vue
@@ -4,12 +4,21 @@
             <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
                 <NuxtLink :to="{ name: 'a-empresa' }">A empresa</NuxtLink>
                 <NuxtLink to="/servicos">Serviços</NuxtLink>
+                <NuxtLink to="/produtos">Produtos</NuxtLink>
             </div>
         </div>
         
         <div>
             <div class="container mx-auto">
                 <h1 class="text-lg font-bold">Serviços</h1>
+
+                <NuxtLink to="/servicos/desenvolvimento-de-sites">
+                    Desenvolvimento de sites
+                </NuxtLink>
+
+                <NuxtLink to="/servicos/marketing-digital">
+                    Marketing digital
+                </NuxtLink>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus diam vitae ante tincidunt, et blandit tellus pharetra. Suspendisse lobortis enim in orci egestas consectetur. Pellentesque a hendrerit nulla. Sed pulvinar nunc diam, ut blandit diam tristique vel. Suspendisse eu aliquet justo. In malesuada odio quis fermentum varius. Vestibulum tempus, sem eget consequat commodo, ex magna consequat dui, et ultrices libero erat ac dui. </p>
             </div>
         </div>
