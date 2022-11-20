0b4bbd8d6324a87c65c014859b25285ca6e49347
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2022-11-20 04:06:18 -0300

    #10 - Vuex

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
     <Nuxt />
-    <Tutorial />
+    
+    <pre>
+      {{ $store.state.user }}
+    </pre>
+
+    <pre>
+      {{ user }}
+    </pre>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapState } from 'vuex';
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  computed: {
+    ...mapState({
+      user: state => state.user,
+    })
+  },
 })
 </script>
---
+++ store/user/index.js
@@ -0,0 +1,5 @@
+export const state = () => ({
+    firstName: 'Jon',
+    lastname: 'Snow',
+    courses: [],
+})
